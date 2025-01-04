8f404c5ab148001889cff4f6de5bc9fd39caa6b3
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-04 22:03:53 +0000

    chore:

---
+++ packages/ui/src/components/card.tsx
@@ -0,0 +1,9 @@
+const Card = () => {
+  return (
+    <div className="bg-green-500">
+      This card is imported
+    </div>
+  )
+};
+
+export default Card;
---
+++ packages/ui/src/sections/section.tsx
@@ -0,0 +1,11 @@
+import Card from "../components/card";
+const Section = () => {
+  return (
+    <div className="bg-blue-500">
+      This section is imported
+      <Card />
+    </div>
+  )
+};
+
+export default Section;

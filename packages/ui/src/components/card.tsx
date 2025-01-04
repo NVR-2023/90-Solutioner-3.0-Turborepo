4a6e85b52499384892a88f7ed6c84fc830131d86
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2025-01-04 22:29:47 +0000

    chore:

--- packages/ui/src/components/card.tsx
+++ packages/ui/src/components/card.tsx
@@ -2,7 +2,7 @@ const Card = () => {
   return (
     <div className="bg-green-500">
       This card is imported
-      <div className="space-y-10">
+      <div className="space-y-18">
         <p className="bg-yellow-500">123</p>
         <p className="bg-yellow-500">123</p>
         <p className="bg-yellow-500">123</p>
